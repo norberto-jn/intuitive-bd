3f0665f68c8ac2300b40678a9b31eea13939b29e
norberto-jn <norbertojunio@gmail.com>
2025-04-03 17:57:22 -0300

    fix: Ajustes na consulta de busca do exercício 3.5

--- 3.5.repostas.sql
+++ 3.5.repostas.sql
@@ -4,20 +4,20 @@
 */
 
 SELECT 
-    f.reg_ans,
-    hip.razao_social AS nome_operadora,
-    SUM(f.vl_saldo_final - f.vl_saldo_inicial) AS despesa_total,
+    f.ans_registration_code,
+    hip.socialname AS nome_operadora,
+    SUM(f.final_balance_amount - f.initial_balance_amount) AS despesa_total,
     CURRENT_DATE - INTERVAL '3 months' AS data_inicio,
     CURRENT_DATE AS data_fim
 FROM 
-    financial_data f
-LEFT JOIN 
-    intuitivecare.health_insurance_providers hip ON f.reg_ans = hip.registro_ans
+    intuitivecare.financial_data f
+INNER JOIN 
+    intuitivecare.health_insurance_providers hip ON f.ans_registration_code = hip.ans_registration_code
 WHERE 
-    f.descricao ILIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
-    AND f.data BETWEEN CURRENT_DATE - INTERVAL '3 months' AND CURRENT_DATE
+    f.description ILIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
+    AND f.date BETWEEN CURRENT_DATE - INTERVAL '3 months' AND CURRENT_DATE
 GROUP BY 
-    f.reg_ans, hip.razao_social
+    f.ans_registration_code, hip.socialname
 ORDER BY 
     despesa_total DESC
 LIMIT 10;
@@ -27,20 +27,55 @@ LIMIT 10;
 */
 
 SELECT 
-    f.reg_ans,
-    hip.razao_social AS nome_operadora,
-    hip.nome_fantasia,
-    SUM(f.vl_saldo_final - f.vl_saldo_inicial) AS despesa_total,
-    EXTRACT(YEAR FROM CURRENT_DATE) - 1 AS ano_referencia
+    f.ans_registration_code,
+    hip.socialname,
+    hip.fantasyname,
+    SUM(f.final_balance_amount - f.initial_balance_amount) AS despesa_total,
+    EXTRACT(YEAR FROM CURRENT_DATE) - 1 AS reference_year
 FROM 
-    financial_data f
-LEFT JOIN 
-    intuitivecare.health_insurance_providers hip ON f.reg_ans = hip.registro_ans
+    intuitivecare.financial_data f
+INNER JOIN 
+    intuitivecare.health_insurance_providers hip ON f.ans_registration_code = hip.ans_registration_code
 WHERE 
-    f.descricao ILIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
-    AND EXTRACT(YEAR FROM f.data) = EXTRACT(YEAR FROM CURRENT_DATE) - 1
+    f.description ILIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
+    AND EXTRACT(YEAR FROM f.date) = EXTRACT(YEAR FROM CURRENT_DATE) - 1
 GROUP BY 
-    f.reg_ans, hip.razao_social, hip.nome_fantasia
+    f.ans_registration_code, hip.socialname, hip.fantasyname
 ORDER BY 
     despesa_total DESC
+LIMIT 10;
+
+
+/*
+    ====================================================================================
+    ====================================================================================
+    ====================================================================================
+*/
+
+
+/*
+    Quais são as 10 operadoras com maiores despesas em 'EVENTOS/SINISTROS CONHECIDOS OU
+    AVISADOS DE ASSISTÊNCIA À SAÚDE MÉDICO-HOSPITALAR' no último trimestre do último ano?
+*/
+
+SELECT 
+    f.ans_registration_code,
+    hip.socialname,
+    SUM(f.final_balance_amount - f.initial_balance_amount) AS total_expense,
+    DATE_TRUNC('year', CURRENT_DATE) - INTERVAL '1 year' + INTERVAL '9 months' AS begin_date,
+    DATE_TRUNC('year', CURRENT_DATE) - INTERVAL '1 day' AS end_date
+FROM 
+    intuitivecare.financial_data f
+INNER JOIN 
+    intuitivecare.health_insurance_providers hip ON f.ans_registration_code = hip.ans_registration_code
+WHERE 
+    f.description ILIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
+    AND f.date BETWEEN 
+        (DATE_TRUNC('year', CURRENT_DATE) - INTERVAL '1 year' + INTERVAL '9 months') -- 1º de outubro do ano anterior
+        AND 
+        (DATE_TRUNC('year', CURRENT_DATE) - INTERVAL '1 day') -- 31 de dezembro do ano anterior
+GROUP BY 
+    f.ans_registration_code, hip.socialname
+ORDER BY 
+    total_expense DESC
 LIMIT 10;
